7f9539531371f6ca02b20a94eaa15d6b3d15fe02
sanaz <sanaz.shahmoradi1385@gmail.com>
2026-08-03 09:38:47 +0330

    tamrinjs

--- tamrin.js
+++ tamrin.js
@@ -1,45 +1,78 @@
-// tamrin 1
-var number = 15;
-if (number % 2 === 0){
-    console.log("number is even");
-} else {
-    console.log("number is odd");
-}
+// // tamrin 1
+// var number = 18;
+// if (number % 2 === 0){
+//     console.log("number is even");
+// } else {
+//     console.log("number is odd");
+// }
 
-// tamrin 2
-var cart = [
-    {
-        name: "mobile",
-        price: 85,
-    },
-    {
-        name: "laptop",
-        price: 150,
-    },
-    {
-        name: "muse",
-        price: 25,
-    },
-];
-var sumprice = cart[0].price + cart[1].price + cart[2].price;
-console.log(sumprice);
-if (sumprice >100){
-    console.log("discount applied");
-} else{
-    console.log("no discount");
-}
+// // tamrin 2
+// var cart = [
+//     {
+//         name: "mobile",
+//         price: 85,
+//     },
+//     {
+//         name: "laptop",
+//         price: 150,
+//     },
+//     {
+//         name: "muse",
+//         price: 25,
+//     },
+// ];
+// var sumprice = cart[0].price + cart[1].price + cart[2].price;
+// console.log(sumprice);
+// if (sumprice >100){
+//     console.log("discount applied");
+// } else{
+//     console.log("no discount");
+// }
 
-// tamrin 3
-var weakday =5;
-switch (weakday){
-    case 3:
-    case 4:
-        console.log("soon weakend");
-        break;
-    case 5:
-    case 6:
-        console.log("weakend");
-        break;
-    default:
-        console.log("looking for weakend");
-}
+// //tamrin 3 loop
+// for (var i = 0 ; i < 5 ; i++ ){
+//     console.log(i);
+// }
+
+// // tamrin 4
+// var weekday =5;
+// switch (weakday){
+//     case 3:
+//     case 4:
+//         console.log("soon weekend");
+//         break;
+//     case 5:
+//     case 6:
+//         console.log("weekend");
+//         break;
+//     default:
+//         console.log("looking for weekend");
+// }
+
+// var number = [2, 6, 8, 3, 5, 7, 2, 5, 2, 5, 2];
+// var count = {};
+// for (var i = 0; i < number.length; i++) {
+//   if (count[number[i]]) {
+//     count[number[i]] = count[number[i]] + 1;
+//   } else {
+//     count[number[i]] = 1;
+//   }
+// }
+// console.log(count);
+
+// var numbers = [15, 5, 12, 6, 8, 19];
+// var maxEven = null;
+// var minodd = null;
+// for (var i = 0; i < numbers.length; i++) {
+//   if (numbers[i] % 2 === 0) {
+//     if (maxEven === null || numbers[i] > maxEven) {
+//       maxEven = numbers[i];
+//     }
+//   } else {
+//     if (minodd === null || numbers[i] < minodd) {
+//       minodd = numbers[i];
+//     }
+//   }
+// }
+// console.log(maxEven, "even");
+// console.log(minodd, "odd");
