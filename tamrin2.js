42da4eb0a2bf1adbb304a887e7105d24b91bd575
sanaz <sanaz.shahmoradi1385@gmail.com>
2026-08-03 09:57:29 +0330

    add tamrin2

---
+++ tamrin2.js
@@ -0,0 +1,33 @@
+// practice1
+// var students = [
+//   {
+//     name: "ali",
+//     course: ["html", "css"],
+//   },
+//   {
+//     name: "amir",
+//     course: ["js", "react"],
+//   },
+// ];
+// for (var i = 0; i < students.length; i++) {
+//   var result = students[i].name + "=";
+//   for (var a = 0; a < students[i].course.length; a++) {
+//     result += students[i].course[a];
+//     if (a < students[i].course.length - 1) {
+//       result += "&";
+//     }
+//   }
+//   console.log(result);
+// }
+
+//practice2
+// for (var i = 1; i <= 10; i++) {
+//   for (var a = 1; a <= 10; a++) {
+//     console.log(i + "*" + a + "=" + i * a);
+//   }
+// }
+
+//practice3
+var birthyear = 2006;
+var age = 2026 - birthyear;
+console.log(age);
